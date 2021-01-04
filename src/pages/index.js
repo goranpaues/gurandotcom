447accbac02aa084f179d447aaa5f8f8028a9c47
Göran Paues <goran.paues@tradedoubler.com>
2021-01-04 14:26:10 +0100

    Changed title

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,6 @@ class IndexPage extends React.Component {
           keywords={[`blog`, `database`, `oracle`, `sql`, `postgresql`, `postgres`, `tips`]}
         />
         <img style={{ margin: 0 }} src="./GoranPaues.jpg" alt="Gatsby Scene" />
-        <script language="JavaScript">
-        (function(i,s,o,g,r,a,m){i['TDConversionObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script', 'https://svht.tradedoubler.com/tr_sdk.js', 'tdconv');
-        </script>
         <h1>
           Göran Paues - Database Developer
         </h1>
