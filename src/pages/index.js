876f74aa34301653ed5e3a3a12e890adfd3e3ec5
Göran Paues <goran.paues@tradedoubler.com>
2021-01-04 14:02:41 +0100

    Removed TD comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,11 +16,9 @@ class IndexPage extends React.Component {
           keywords={[`blog`, `database`, `oracle`, `sql`, `postgresql`, `postgres`, `tips`]}
         />
         <img style={{ margin: 0 }} src="./GoranPaues.jpg" alt="Gatsby Scene" />
-        <!-- Start TradeDoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->
         <script language="JavaScript">
         (function(i,s,o,g,r,a,m){i['TDConversionObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script', 'https://svht.tradedoubler.com/tr_sdk.js', 'tdconv');
         </script>
-        <!-- End TradeDoubler tag-->
         <h1>
           Göran Paues - Database Developer
         </h1>
